cc00c3f915f5fca48fbfb5cbfd433fb40e532f5e
HarryHawkins <harry@harryhawkins.com>
2020-05-17 13:09:57 +0100

    added some simple validation for films that cannot be found

--- common-actors.py
+++ common-actors.py
@@ -8,20 +8,37 @@ movie_B = input("Enter your second movie: ")
 #find movies with similar names to users input
 search_results_A = ia.search_movie(movie_A)
 search_results_B = ia.search_movie(movie_B)
-
+if not search_results_A:
+    print("No movie found called :",movie_A)
+    print("Please try again")
+if not search_results_B:
+    print("No movie found called :",movie_B)
+    print("Please try again")
 if search_results_A and search_results_B:
     #movie A is set to first result
     movieID_A = search_results_A[0].movieID
     movie_object_A = ia.get_movie(movieID_A)
-    print(movie_object_A.get('title'))
+    print("First movie found: ",movie_object_A.get('title'))
     #movie B is set to first result
     movieID_B = search_results_B[0].movieID
     movie_object_B = ia.get_movie(movieID_B)
-    print(movie_object_B.get('title'))
+    print("Second movie found: ",movie_object_B.get('title'))
 
     #get casts and find common cast members
     cast_A = movie_object_A.get('cast')
     cast_B = movie_object_B.get('cast')
-    common_actors = list(set(cast_A)&set(cast_B))
-    for actor in common_actors:
-        print(actor.get('name'))
+    if cast_A and cast_B:
+        common_actors = list(set(cast_A)&set(cast_B))
+        if common_actors:
+            print("Common actors found ", len(common_actors), ": ")
+            for actor in common_actors:
+                print(actor.get('name'))
+        else:
+            print("No common actors")
+    else:
+        if not cast_A:
+            print("Cast not found for ",movie_object_A.get('title'))
+        if not cast_B:
+            print("Cast not found for ",movie_object_A.get('title'))
+
+
