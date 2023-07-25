990ee88fe830d9a809495ed66566f624230a2e8e
Harry Hawkins <41339027+HarryHawkins@users.noreply.github.com>
2023-07-25 13:57:11 +0100

    Update common-actors.py
    refactored

--- common-actors.py
+++ common-actors.py
@@ -1,88 +1,85 @@
-import sys
 from imdb import IMDb
 
 
-def main():
-    # create an instance of the IMDb class
-    ia = IMDb()
-    # get movies from user
-    print("Welcome to common-actors! \nThis tool will give you the common actors from a selection of given movies\n")
+def get_number_of_movies():
     while True:
         try:
-            num_movies = int(
-                input("How many movies would you like to enter? "))
+            num_movies = int(input("How many movies would you like to enter? "))
         except ValueError:
             print("Please enter a number!")
-            # better try again... Return to the start of the loop
             continue
         else:
             break
+    return num_movies
 
+
+def get_movie_names(num_movies):
     movies = []
     for i in range(num_movies):
-        movies.append(input("Enter movie number "+str(i+1) + ": "))
+        movies.append(input(f"Enter movie number {i+1}: "))
+    return movies
 
-    # find movies with similar names to users input
+
+def get_search_results(ia, movies):
     search_results = []
     for movie in movies:
         search_results.append(ia.search_movie(movie))
+    return search_results
 
-    # check we got results for each input
+
+def check_search_results(search_results, movies):
     missing_result = False
-    i = 0
-    for result in search_results:
+    for i, result in enumerate(search_results):
         if not result:
-            print("No movie found called : "+movies[i])
+            print(f"No movie found called : {movies[i]}")
             print("Please try again")
             missing_result = True
-        i += 1
+    return missing_result
 
-    if missing_result == False:
-        # get ID of each movie and assign to objects (assume first result is the movie wanted)
-        movie_IDs = []
-        for result in search_results:
-            movie_IDs.append(result[0].movieID)
 
-        # get movie objects for each ID
-        movie_objects = []
-        for id in movie_IDs:
-            movie_objects.append(ia.get_movie(id))
-            print("Movie " + str(len(movie_objects)) +
-                  " found: "+ia.get_movie(id).get('title'))
+def get_movie_objects_and_casts(ia, search_results):
+    movie_objects = []
+    movie_casts = []
+    for result in search_results:
+        movie = ia.get_movie(result[0].movieID)
+        movie_objects.append(movie)
+        print(f"Movie {len(movie_objects)} found: {movie.get('title')}")
+        movie_casts.append(movie.get('cast'))
+    return movie_objects, movie_casts
 
-        # get cast for each movie
-        movie_casts = []
-        for obj in movie_objects:
-            movie_casts.append(obj.get('cast'))
 
-        # check we got a cast for each movie
-        cast_exist = True
-        i = 0
-        for cast in movie_casts:
-            if not cast:
-                cast_exist = False
-                print("Unfortunately there is no cast for " +
-                      movie_objects[i].get('title'))
-                print("Please try again with a different movie\n")
-            i += 1
+def check_casts(movie_objects, movie_casts):
+    cast_exist = True
+    for i, cast in enumerate(movie_casts):
+        if not cast:
+            cast_exist = False
+            print(f"Unfortunately there is no cast for {movie_objects[i].get('title')}")
+            print("Please try again with a different movie\n")
+    return cast_exist
 
+
+def print_common_actors(movie_casts):
+    common_actors = set.intersection(*map(set, movie_casts))
+    if common_actors:
+        print(f"Common actors found {len(common_actors)}: ")
+        for actor in common_actors:
+            print(actor.get('name'))
+    else:
+        print("No common actors")
+
+
+def main():
+    ia = IMDb()
+    print("Welcome to common-actors! \nThis tool will give you the common actors from a selection of given movies\n")
+    num_movies = get_number_of_movies()
+    movies = get_movie_names(num_movies)
+    search_results = get_search_results(ia, movies)
+    missing_result = check_search_results(search_results, movies)
+    if not missing_result:
+        movie_objects, movie_casts = get_movie_objects_and_casts(ia, search_results)
+        cast_exist = check_casts(movie_objects, movie_casts)
         if cast_exist:
-            # get intersection of all movie casts (common actors)
-            common_actors = set.intersection(*map(set, movie_casts))
-            if common_actors:
-                print("Common actors found " + str(len(common_actors)) + ": ")
-                for actor in common_actors:
-                    print(actor.get('name'))
-                    if len(sys.argv) > 1:
-                        if sys.argv[1] == '-wiki':
-                            import webbrowser
-                            # makes annoying output in CLI, but not much of an issue
-                            webbrowser.open(
-                                'https://en.wikipedia.org/wiki/'+actor.get('name'))
-            else:
-                print("No common actors")
-        else:
-            main()
+            print_common_actors(movie_casts)
 
 
 if __name__ == "__main__":
